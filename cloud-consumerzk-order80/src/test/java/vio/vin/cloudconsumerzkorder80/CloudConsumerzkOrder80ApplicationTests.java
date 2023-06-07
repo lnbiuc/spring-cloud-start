aecf32e74b520941f3ef89cd5cb87b92e1a2a070
lnbiuc <15771670073@163.com>
2023-06-07 22:04:53 +0800

    feat: use zookeeper

---
+++ cloud-consumerzk-order80/src/test/java/vio/vin/cloudconsumerzkorder80/CloudConsumerzkOrder80ApplicationTests.java
@@ -0,0 +1,13 @@
+package vio.vin.cloudconsumerzkorder80;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConsumerzkOrder80ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
