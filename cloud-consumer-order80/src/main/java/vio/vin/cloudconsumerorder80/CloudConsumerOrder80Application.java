356c2b1271847165e8bd56be04bba59070e6439f
lnbiuc <15771670073@163.com>
2023-06-11 19:54:53 +0800

    feat: ribbon use custom rule

--- cloud-consumer-order80/src/main/java/vio/vin/cloudconsumerorder80/CloudConsumerOrder80Application.java
+++ cloud-consumer-order80/src/main/java/vio/vin/cloudconsumerorder80/CloudConsumerOrder80Application.java
@@ -3,9 +3,12 @@ package vio.vin.cloudconsumerorder80;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import vio.vin.rule.RibbonRule;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = RibbonRule.class)
 public class CloudConsumerOrder80Application
 {
 
