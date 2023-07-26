106c0d77ae9e5d8fd42b239ae1cd9cfbd18d22b9
violet <violetzzs@proton.me>
2023-07-26 23:40:08 +0800

    :sparkles: use spring gateway

---
+++ cloud-gateway-gateway9527/src/test/java/vio/vin/cloudgatewaygateway9527/CloudGatewayGateway9527ApplicationTests.java
@@ -0,0 +1,13 @@
+package vio.vin.cloudgatewaygateway9527;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayGateway9527ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
