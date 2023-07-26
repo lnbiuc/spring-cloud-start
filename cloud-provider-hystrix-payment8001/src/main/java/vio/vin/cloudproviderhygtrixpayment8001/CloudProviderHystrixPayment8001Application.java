0b4667d0f5ba41a5fcb8e5f106db40d93579b083
violet <violetzzs@proton.me>
2023-07-26 17:11:18 +0800

    :sparkles: 8001 enable CircuitBreaker

--- cloud-provider-hystrix-payment8001/src/main/java/vio/vin/cloudproviderhygtrixpayment8001/CloudProviderHystrixPayment8001Application.java
+++ cloud-provider-hystrix-payment8001/src/main/java/vio/vin/cloudproviderhygtrixpayment8001/CloudProviderHystrixPayment8001Application.java
@@ -2,10 +2,12 @@ package vio.vin.cloudproviderhygtrixpayment8001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker // 开启服务降级
 public class CloudProviderHystrixPayment8001Application
 {
 
