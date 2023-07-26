fbeaf799a4fed5249787aee6f8a5c7798001cd8c
violet <violetzzs@proton.me>
2023-07-26 23:19:48 +0800

    :sparkles: hystrix dashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/test/java/vio/vin/cloudconsumerhystrixdashboard9001/CloudConsumerHystrixDashboard9001ApplicationTests.java
@@ -0,0 +1,15 @@
+package vio.vin.cloudconsumerhystrixdashboard9001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConsumerHystrixDashboard9001ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
