118cfe3f34f08851605527c9c1582fbc7d23201b
violet <violetzzs@proton.me>
2023-07-27 00:53:30 +0800

    :sparkles: use config center

---
+++ cloud-config-center-3344/src/main/java/vio/vin/cloudconfigcenter3344/CloudConfigCenter3344Application.java
@@ -0,0 +1,17 @@
+package vio.vin.cloudconfigcenter3344;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloudConfigCenter3344Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CloudConfigCenter3344Application.class, args);
+    }
+
+}
---
+++ cloud-config-center-3344/src/test/java/vio/vin/cloudconfigcenter3344/CloudConfigCenter3344ApplicationTests.java
@@ -0,0 +1,15 @@
+package vio.vin.cloudconfigcenter3344;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigCenter3344ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
