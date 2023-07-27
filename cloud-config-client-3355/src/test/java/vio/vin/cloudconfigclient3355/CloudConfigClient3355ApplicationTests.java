bd046ee15f24d37330e69896e87c1c6aa38277ec
violet <violetzzs@proton.me>
2023-07-27 22:57:19 +0800

    :sparkles: add config 3355

---
+++ cloud-config-client-3355/src/test/java/vio/vin/cloudconfigclient3355/CloudConfigClient3355ApplicationTests.java
@@ -0,0 +1,15 @@
+package vio.vin.cloudconfigclient3355;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigClient3355ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
