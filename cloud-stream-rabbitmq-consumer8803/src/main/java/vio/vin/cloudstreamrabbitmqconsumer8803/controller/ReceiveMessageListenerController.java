f1899384c9f0d61c3d93cb0d1bea2a92ac0749c6
violet <violetzzs@proton.me>
2023-07-28 21:55:00 +0800

    :sparkles: consumer message 8803

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/vio/vin/cloudstreamrabbitmqconsumer8803/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,25 @@
+package vio.vin.cloudstreamrabbitmqconsumer8803.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableBinding(Sink.class)
+@Slf4j
+public class ReceiveMessageListenerController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message)
+    {
+        log.info("{} *********** receive message : {}", serverPort, message.getPayload());
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/test/java/vio/vin/cloudstreamrabbitmqconsumer8803/CloudStreamRabbitmqConsumer8803ApplicationTests.java
@@ -0,0 +1,15 @@
+package vio.vin.cloudstreamrabbitmqconsumer8803;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStreamRabbitmqConsumer8803ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
