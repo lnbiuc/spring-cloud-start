b34bf3a38ee1eb9a777cba5bc6e51415fcd4d935
violet <violetzzs@proton.me>
2023-07-29 23:23:52 +0800

    :sparkles: use nacos config

---
+++ cloudalibaba-config-nacos-client3377/src/test/java/vio/vin/cloudalibabaconfignacosclient3377/CloudalibabaConfigNacosClient3377ApplicationTests.java
@@ -0,0 +1,15 @@
+package vio.vin.cloudalibabaconfignacosclient3377;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudalibabaConfigNacosClient3377ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
